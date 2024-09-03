6488836cd816d3055a065ad4ce9345eac1cc7dc7
ralleyne531 <ralleyne531@gmail.com>
2024-09-02 23:40:19 -0400

    lo-fi structure

--- UI/portfolio/src/App.js
+++ UI/portfolio/src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
+import React from 'react'
 
 function App() {
-
+  const[isSidebarHidden, setIsSidebarHidden] = React.useState(true)
+  const toggleSidebar = () => {
+    if(isSidebarHidden){
+      setIsSidebarHidden(false)
+    }else{
+      setIsSidebarHidden(true)
+    }
+  }
 
   return (
+    <>
     <div className="App">
       <navbar>
         Nav
+        <button onClick={toggleSidebar}>Button</button>
       </navbar>
-      <aside>side</aside>
-      <content>
-        content
-      </content>
+      <aside class={isSidebarHidden ? "" : 'show'}>side</aside>
+      <main>
+      Main
+      </main>
       <footer>footer</footer>
     </div>
+    </>
   );
 }
 
