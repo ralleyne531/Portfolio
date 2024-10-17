235d77fdf2cd8108fbdb6ef60b2b5d8e979d5762
ralleyne531 <ralleyne531@gmail.com>
2024-10-17 01:21:52 -0400

    Placing 'a' element inside of sidebar li

--- UI/portfolio/src/Components/Sidebar.js
+++ UI/portfolio/src/Components/Sidebar.js
@@ -4,11 +4,11 @@ export default function Sidebar(){
 
     return(
         <ul>
-            <li>Home</li>
-            <li>about</li>
-            <li>work</li>
-            <li>interests</li>
-            <li>contact</li>
+            <li><a href={"#"}>Home</a></li>
+            <li><a href={"#"}>about</a></li>
+            <li><a href={"#"}>work</a></li>
+            <li><a href={"#"}>interests</a></li>
+            <li><a href={"#"}>contact</a></li>
         </ul>
     )
 }
