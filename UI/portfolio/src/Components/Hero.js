151515b4fe9c1f5715829b66c0b5a7ec3ed58bb1
ralleyne531 <ralleyne531@gmail.com>
2025-03-27 20:23:29 -0400

    Creating basic section component

---
+++ UI/portfolio/src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero(){
+
+    return (
+        <>
+            <h2>Welcome to My Portfolio!</h2>
+            <p>This is the hero section where I introduce myself.</p>
+        </>
+    );
+}
+
+export default Hero;
