a768c744acceb5de7690f6d391292487dd2ae95c
ralleyne531 <ralleyne531@gmail.com>
2025-03-29 12:40:36 -0400

    Minor flexbox tweaks

--- UI/portfolio/src/Components/ProjectsComponent/ProjectsCard.js
+++ UI/portfolio/src/Components/ProjectsComponent/ProjectsCard.js
@@ -6,6 +6,10 @@ const ProjectsCardDiv = styled.div`
   
   display: flex;
   flex-direction: column;
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  
 `
 
 function ProjectsCard({project,creationDate,stage,iconNames}){
@@ -18,14 +22,14 @@ function ProjectsCard({project,creationDate,stage,iconNames}){
             <h4>Hello World: {creationDate}</h4>
             <p>Stage: {stage}</p>
             {/* Render icons dynamically */}
-            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', }}>
                 {iconNames && iconNames.map((iconName) => {
                     try {
                         // Dynamically load the icon component
                         const Icon = importIcons(`./${iconName}.js`).default;
                         return (
-                            <div key={iconName} style={{ textAlign: 'center', margin: '10px' }}>
-                                <Icon width={5} height={5} />
+                            <div key={iconName} style={{ textAlign: 'center' }}>
+                                <Icon/>
                             </div>
                         );
                     } catch (error) {
