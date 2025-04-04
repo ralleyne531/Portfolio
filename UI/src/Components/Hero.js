b2d3ff675df60e244ecb82a4ecf6385ed061da8e
ralleyne531 <ralleyne531@gmail.com>
2025-04-04 03:11:23 -0400

    Increasing font accessibility, fixing typos and Safari styling

--- UI/src/Components/Hero.js
+++ UI/src/Components/Hero.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from "styled-components";
 
 const StyledHero = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   border-bottom: black 1px solid;
   padding: 3em;
   text-align: center;
