775f56cebae499ede0a7d788a1256a0f3cca185c
ralleyne531 <ralleyne531@gmail.com>
2025-07-06 15:14:49 -0400

    Replacing main font + styling overhaul

--- UI/src/Components/HeaderComponent/Header.js
+++ UI/src/Components/HeaderComponent/Header.js
@@ -15,7 +15,7 @@ function Header({ toggleSidebar }) {
         <div className="header">
             <h5>Rashidi Alleyne</h5>
             <ContactStatus className={"contactMe"}></ContactStatus>
-            <svg onClick={sidebarAnimation} className={isMerged? "merged" : ""} id="grid" fill="#000000" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g id="squares">
+            <svg onClick={sidebarAnimation} className={isMerged? "merged" : ""} id="grid" fill="#EEEEEE" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><g id="squares">
                 <rect className="square" x="0" y="0" width="4" height="4"/>
                 <rect className="square" x="6" y="0" width="4" height="4"/>
                 <rect className="square" x="12" y="0" width="4" height="4"/>
